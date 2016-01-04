30417cc412e28c3ef5343ca05eeb1dcc9dd08419
madhephaestus <mad.hephaestus@gmail.com>
2016-01-03 22:02:58 -0500

    fixing javadoc

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/ICadGenerator.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/ICadGenerator.java
@@ -13,7 +13,7 @@ public interface ICadGenerator {
 	/**
 	 * This function should use the D-H parameters to generate cad objects to build this configuration
 	 * the user should attach any listeners from the DH link for simulation
-	 * @param dhLinks the list of DH configurations
+	 * @param dh the list of DH configurations
 	 * @return simulatable CAD objects
 	 */
 	ArrayList<CSG> generateCad(DHParameterKinematics dh, boolean toManufacture );
