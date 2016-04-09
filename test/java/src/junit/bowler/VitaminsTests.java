17434d1fff3427fa59c5267931f4f4a88a146a53
madhephaestus <mad.hephaestus@gmail.com>
2016-04-09 10:56:54 -0400

    adding vitamins unit tests

---
+++ test/java/src/junit/bowler/VitaminsTests.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package junit.bowler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author hephaestus
+ *
+ */
+public class VitaminsTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
