ed006d3932f6b5f0f26439c98faaa47e458a2a07
madhephaestus <mad.hephaestus@gmail.com>
2016-04-10 17:37:03 -0400

    adding github watching list

--- test/java/src/junit/bowler/GitHub.java
+++ test/java/src/junit/bowler/GitHub.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
+import org.kohsuke.github.GHMyself;
 import org.kohsuke.github.GHOrganization;
 import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHTeam;
+import org.kohsuke.github.PagedIterable;
 
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
 import com.neuronrobotics.sdk.util.ThreadUtil;
@@ -32,7 +36,30 @@ public class GitHub {
 				System.out.println("\tRepo "+org+" "+orgRepo);
 			}
 		}
-		
+		Map<String, Set<GHTeam>> teams = github.getMyTeams();
+		for (String team :teams.keySet()){
+			System.out.println("Team "+team);
+			Set<GHTeam> ghteam = teams.get(team);
+			for(GHTeam ghT: ghteam){
+				System.out.println("\tGHTeam "+ghT.getName());
+				Map<String, GHRepository> repos = ghT.getRepositories();
+				for(String repoName:repos.keySet()){
+					System.out.println("\t\tGHTeam "+ghT.getName()+" repo "+repoName);
+				}
+			}
+		}
+		GHMyself self = github.getMyself();
+		Map<String, GHRepository> myPublic = self.getAllRepositories();
+		for (String myRepo :myPublic.keySet()){
+			System.out.println("Repo "+myRepo);
+			GHRepository ghrepo= myPublic.get(myRepo);
+			//if(ghrepo.getOwnerName().contains("demo"))
+			System.out.println("\tOwner: "+ghrepo.getOwnerName()+" "+myRepo);
+		}
+		PagedIterable<GHRepository> watching = self.listSubscriptions();
+		for(GHRepository g:watching){
+			System.out.println("Watching "+g.getOwnerName()+" "+g.getFullName());
+		}
 	}
 
 }
