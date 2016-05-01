af9d24b5167882e1f1f8da3c7a01bfcde9bb8215
madhephaestus <mad.hephaestus@gmail.com>
2016-05-01 12:48:45 -0400

    Jetty test seems working

--- test/java/src/junit/bowler/JettyTest.java
+++ test/java/src/junit/bowler/JettyTest.java
@@ -8,76 +8,96 @@ import java.net.URL;
 import java.util.Scanner;
 
 import org.apache.commons.io.IOUtils;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
 import com.neuronrobotics.sdk.util.ThreadUtil;
 
 public class JettyTest {
-	private static final int WEBSERVER_PORT = 8065;
-	private static String HOME_Local_URL = "http://localhost:"+WEBSERVER_PORT+"/BowlerStudio/Welcome-To-BowlerStudio/";
-	@Test
-	public void test() throws InvalidRemoteException, TransportException, GitAPIException, IOException {
-		try{
+	private static  int WEBSERVER_PORT = 8065;
+	private static String HOME_Local_URL = "http://localhost:" + WEBSERVER_PORT
+			+ "/BowlerStudio/Welcome-To-BowlerStudio/";
+
+	private Server server;
+
+	@Before
+	public void startJetty() throws Exception {
+		try {
 			ScriptingEngine.setupAnyonmous();
-			//ScriptingEngine.setAutoupdate(true);
-		}catch (Exception ex){
+			// ScriptingEngine.setAutoupdate(true);
+		} catch (Exception ex) {
 			System.out.println("User not logged in, test can not run");
 		}
-		File indexOfTutorial = ScriptingEngine.fileFromGit(
-				"https://github.com/NeuronRobotics/NeuronRobotics.github.io.git", 
-				"index.html");
+		File indexOfTutorial = ScriptingEngine
+				.fileFromGit("https://github.com/NeuronRobotics/NeuronRobotics.github.io.git", "index.html");
+
+		// HOME_Local_URL = indexOfTutorial.toURI().toString().replace("file:/",
+		// "file:///");
+		server = new Server();
+		ServerConnector connector = new ServerConnector(server);  
+		server.setConnectors(new Connector[] { connector });
+
 		
-		//HOME_Local_URL = indexOfTutorial.toURI().toString().replace("file:/", "file:///");
-		Server server = new Server(WEBSERVER_PORT);
 
 		ResourceHandler resource_handler = new ResourceHandler();
 		resource_handler.setDirectoriesListed(true);
 		resource_handler.setWelcomeFiles(new String[] { "index.html" });
-		System.out.println("Serving "+ indexOfTutorial.getParent());
+		System.out.println("Serving " + indexOfTutorial.getParent());
 		resource_handler.setResourceBase(indexOfTutorial.getParent());
 
 		HandlerList handlers = new HandlerList();
 		handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
 		server.setHandler(handlers);
-		new Thread(){
-			public void run(){
-				try {
-					server.start();
-					server.join();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					fail(e.getMessage());
-				}
-			}
-		}.start();
-		ThreadUtil.wait(100);
+
+		// Start Server
+		server.start();
+		WEBSERVER_PORT= connector.getLocalPort();
+		HOME_Local_URL = "http://localhost:" + WEBSERVER_PORT
+				+ "/BowlerStudio/Welcome-To-BowlerStudio/";
+	}
+
+	@Test
+	public void test() throws InvalidRemoteException, TransportException, GitAPIException, IOException {
+
 		try {
-				java.io.InputStream url = new URL("http://127.0.0.1:"+WEBSERVER_PORT+"/index.html").openStream();
-			   try {
-			       System.out.println(org.apache.commons.io.IOUtils.toString( url )) ;
-			    } finally {
-			    	org.apache.commons.io.IOUtils.closeQuietly(url);
-			    }
-			   // read from your scanner
+			java.io.InputStream url = new URL(HOME_Local_URL).openStream();
+			try {
+				System.out.println(org.apache.commons.io.IOUtils.toString(url));
+			} finally {
+				org.apache.commons.io.IOUtils.closeQuietly(url);
 			}
-			catch(IOException ex) {
-			   // there was some connection problem, or the file did not exist on the server,
-			   // or your URL was not in the right format.
-			   // think about what to do now, and put it here.
-			   ex.printStackTrace(); // for now, simply output it.
-			   fail(ex.getMessage());
-			}
-		server.destroy();
+
+			// read from your scanner
+		} catch (IOException ex) {
+			// there was some connection problem, or the file did not exist on
+			// the server,
+			// or your URL was not in the right format.
+			// think about what to do now, and put it here.
+			ex.printStackTrace(); // for now, simply output it.
+			fail(ex.getMessage());
+		}
+
+	}
+
+	@After
+	public void stopJetty() {
+		try {
+			server.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
