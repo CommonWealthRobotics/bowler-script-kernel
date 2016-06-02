9358e68c13e2aac6c9f878d2eda8518acbbf0e35
madhephaestus <mad.hephaestus@gmail.com>
2016-06-02 10:39:52 -0400

    adding weka library

---
+++ test/java/src/junit/bowler/VoiceTest.java
@@ -0,0 +1,16 @@
+package junit.bowler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.neuronrobotics.bowlerstudio.BowlerKernel;
+
+public class VoiceTest {
+
+	@Test
+	public void test() {
+		BowlerKernel.speak("Testing voice");
+	}
+
+}
