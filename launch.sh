a195564729d1afbcfb7206b4aa6e80d0837ac4f0
Kevin Harrington <mad.hephaestus@gmail.com>
2019-07-03 16:21:24 -0400

    no copy

--- launch.sh
+++ launch.sh
@@ -7,5 +7,7 @@ rm ~/.github
 rm -rf ~/bowler-workspace
 java -jar build/libs/BowlerScriptingKernel-*-fat.jar -g https://github.com/OperationSmallKat/greycat.git launch.groovy 
 
+exit 0
+
 rm -rf /media/hephaestus/9a5619d0-38d6-4005-89ba-72d1e34dd71f/home/pi/*.jar
 cp build/libs/BowlerScriptingKernel-*-fat.jar /media/hephaestus/9a5619d0-38d6-4005-89ba-72d1e34dd71f/home/pi/
