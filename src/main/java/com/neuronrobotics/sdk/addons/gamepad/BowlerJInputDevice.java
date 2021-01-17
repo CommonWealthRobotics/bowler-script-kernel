4b7eb8111942d4d65f6590de3900a5f087765570
Kevin Harrington <mad.hephaestus@gmail.com>
2021-01-17 15:58:19 -0500

    Pached over the class files of JInput
    now getting controllers rescans the hardware

--- src/main/java/com/neuronrobotics/sdk/addons/gamepad/BowlerJInputDevice.java
+++ src/main/java/com/neuronrobotics/sdk/addons/gamepad/BowlerJInputDevice.java
@@ -1,19 +1,15 @@
 package com.neuronrobotics.sdk.addons.gamepad;
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import net.java.games.input.Component;
 import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
 import net.java.games.input.Event;
 import net.java.games.input.EventQueue;
-
 import com.neuronrobotics.sdk.common.Log;
 import com.neuronrobotics.sdk.common.NonBowlerDevice;
 import com.neuronrobotics.sdk.util.ThreadUtil;
-import org.apache.commons.lang3.SystemUtils;
-import us.ihmc.tools.nativelibraries.NativeLibraryLoader;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -30,16 +26,22 @@ public class BowlerJInputDevice extends NonBowlerDevice {
 	/** The run. */
 	boolean run = true;
 
+	private HashMap<String, Double> recentValue = new HashMap<String, Double>();
+
 	/** The poller. */
 	private Thread poller;
-	
+
+	private String name;
+	static {
+		net.java.games.input.ControllerEnvironment.getDefaultEnvironment();
+	}
+
 	public static String[] getControllers() {
-		loadLibs();
 		ControllerEnvironment defaultEnvironment = ControllerEnvironment.getDefaultEnvironment();
 		Controller[] getDefaultEnvironmentGetControllers = defaultEnvironment.getControllers();
 		String[] cons = new String[getDefaultEnvironmentGetControllers.length];
-		for(int i=0;i<cons.length;i++) {
-			cons[i]=getDefaultEnvironmentGetControllers[i].getName();
+		for (int i = 0; i < cons.length; i++) {
+			cons[i] = getDefaultEnvironmentGetControllers[i].getName();
 		}
 		return cons;
 	}
@@ -48,32 +50,38 @@ public class BowlerJInputDevice extends NonBowlerDevice {
 	 * Instantiates a new bowler j input device.
 	 */
 	public BowlerJInputDevice(String name) {
-		loadLibs();
+		this.setName(name);
+		setControllerByName(name);
+
+	}
+
+	private void setControllerByName(String name) {
 		ControllerEnvironment defaultEnvironment = ControllerEnvironment.getDefaultEnvironment();
+
 		Controller[] getDefaultEnvironmentGetControllers = defaultEnvironment.getControllers();
-		System.out.println("COntrollers = " + getDefaultEnvironmentGetControllers);
+		if (name == null && getDefaultEnvironmentGetControllers.length > 0) {
+			controller = getDefaultEnvironmentGetControllers[0];
+		} else
+			for (int i = 0; i < getDefaultEnvironmentGetControllers.length; i++) {
+				Controller c = getDefaultEnvironmentGetControllers[i];
+				if (c.getName().toLowerCase().contains(name.toLowerCase())) {
+					System.out.println("Found! " + c.getName());
+					controller = c;
+					this.name=c.getName();
+					controller.poll();
+					EventQueue queue = controller.getEventQueue();
+					Event event = new Event();
+					while (queue.getNextEvent(event)) {
+						// drain startup events
+					}
+					break;
+				} else
+					System.out.println("Non match: " + c.getName() + " " + name);
+			}
 		if (controller != null)
 			this.setController(controller);
 		else
 			throw new RuntimeException("Contoller must not be null");
-
-	}
-
-	public static void loadLibs() {
-		String absolutePathToDirectory;
-		if (SystemUtils.IS_OS_WINDOWS) {
-			absolutePathToDirectory = NativeLibraryLoader.extractLibraries("", "jinput-raw", "jinput-raw_64",
-					"jinput-dx8_64", "jinput-dx8", "jinput-wintab");
-		} else if (SystemUtils.IS_OS_LINUX) {
-			absolutePathToDirectory = NativeLibraryLoader.extractLibraries("", "jinput-linux64", "jinput-linux");
-		} else if (SystemUtils.IS_OS_MAC) {
-			absolutePathToDirectory = new File(NativeLibraryLoader.extractLibraryAbsolute("", "libjinput-osx.jnilib"))
-					.getParent();
-		} else {
-			throw new RuntimeException("Failed to load the native library for JInput!");
-		}
-
-		System.setProperty("net.java.games.input.librarypath", absolutePathToDirectory);
 	}
 
 	/*
@@ -88,6 +96,12 @@ public class BowlerJInputDevice extends NonBowlerDevice {
 		run = false;
 	}
 
+	public double getValue(String component) {
+		if (recentValue.get(component) == null)
+			return 0;
+		return recentValue.get(component);
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -100,39 +114,51 @@ public class BowlerJInputDevice extends NonBowlerDevice {
 				public void run() {
 					setName("Game Controller Poll thread");
 					Log.warning("Starting game Pad Poller");
-					while (run) {
-						boolean pollStat = controller.poll();
-						EventQueue queue = controller.getEventQueue();
-						Event event = new Event();
-						while (queue.getNextEvent(event) && run) {
-//							StringBuffer buffer = new StringBuffer(controller.getName());
-//							buffer.append(" at ");
-//							buffer.append(event.getNanos()).append(", ");
-							Component comp = event.getComponent();
-//							buffer.append(comp.getName()).append(" changed to ");
-							float value = event.getValue();
-//							if (comp.isAnalog()) {
-//								buffer.append(value);
-//							} else {
-//								if (value > 0) {
-//									buffer.append("On");
-//								} else {
-//									buffer.append("Off");
-//								}
-//							}
-//							Log.info(buffer.toString());
-							for (int i = 0; i < listeners.size(); i++) {
-								IGameControlEvent l = listeners.get(i);
-								try {
-									l.onEvent(comp.getName(), value);
-								} catch (Exception ex) {
-									ex.printStackTrace();
+					try {
+						while (run) {
+							if(controller==null) {
+								while (run) {
+									try {
+										setControllerByName(name);
+										break;
+									} catch (Throwable t) {
+										System.out.println("BowlerJInputDevice Waiting for device to be availible");
+										//t.printStackTrace();
+										try {
+											Thread.sleep(1000);
+										} catch (InterruptedException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+									}
 								}
 							}
+							boolean pollStat = controller.poll();
+							if (!pollStat) {
+								controller=null;
+								continue;
+							}
+							EventQueue queue = controller.getEventQueue();
+							Event event = new Event();
+							while (queue.getNextEvent(event) && run) {
+								Component comp = event.getComponent();
+								float value = event.getValue();
+								recentValue.put(comp.getName(), (double) value);
+								for (int i = 0; i < listeners.size(); i++) {
+									IGameControlEvent l = listeners.get(i);
+									try {
+										l.onEvent(comp.getName(), value);
+									} catch (Throwable ex) {
+										ex.printStackTrace();
+									}
+								}
+							}
+							ThreadUtil.wait(16);
 						}
-						ThreadUtil.wait(10);
+					} catch (Throwable t) {
+						t.printStackTrace();
 					}
-					poller=null;
+					disconnect();
 				}
 			};
 			poller.start();
@@ -196,13 +222,43 @@ public class BowlerJInputDevice extends NonBowlerDevice {
 		// TODO Auto-generated method stub
 		return new ArrayList<String>();
 	}
+	
+	@Override
+	public String toString() {
+		String values = "";
+		for(String key:recentValue.keySet()) {
+			values+="\n\t"+key+" = "+recentValue.get(key);
+		}
+		return name+" = "+values;
+	}
 
 	public static void main(String[] args) throws InterruptedException {
 
-		BowlerJInputDevice g = new BowlerJInputDevice(null); // This is the DyIO to talk to.
-		g.connect(); // Connect to it.
-		
-		while(true)Thread.sleep(100);
+		while (true) {
+			try {
+				BowlerJInputDevice g = new BowlerJInputDevice("Gamesir"); // This is the DyIO to talk to.
+				g.connect(); // Connect to it.
+				g.addListeners((name, value) -> {
+					System.out.println(g);
+				});
+				while (g.isAvailable())
+					Thread.sleep(100);
+				System.out.println("Controller clean exit");
+			} catch (Throwable t) {
+				System.out.println("Waiting for device to be availible");
+				t.printStackTrace();
+				Thread.sleep(1000);
+			}
+		}
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
