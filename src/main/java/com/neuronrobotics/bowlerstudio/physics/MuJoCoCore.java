50f0bd8d4eea4df16b27f0b7d3655411c9047d17
Kevin Harrington <kharrington@softroboticsinc.com>
2022-06-02 13:39:13 -0400

    adding compile checks for mujoco

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/physics/MuJoCoCore.java
@@ -0,0 +1,67 @@
+package com.neuronrobotics.bowlerstudio.physics;
+
+import java.util.ArrayList;
+
+import com.bulletphysics.dynamics.DiscreteDynamicsWorld;
+
+import eu.mihosoft.vrl.v3d.CSG;
+
+import org.mujoco.MuJoCoLib;
+
+public class MuJoCoCore implements IPhysicsCore {
+
+	@Override
+	public ArrayList<CSG> getCsgFromEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(IPhysicsManager manager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(IPhysicsManager manager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stepMs(double timeStep) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void step(float timeStep) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopPhysicsThread() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startPhysicsThread(int ms) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public DiscreteDynamicsWorld getDynamicsWorld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
