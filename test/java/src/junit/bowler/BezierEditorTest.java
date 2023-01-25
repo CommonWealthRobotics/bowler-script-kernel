683a57d223f62a17b872cf64b424c0238f9d5d5a
JansenSmith <jansen@bstarengineering.com>
2023-01-25 18:29:20 -0500

    created a unit test placeholder for testing the bezier curve system

---
+++ test/java/src/junit/bowler/BezierEditorTest.java
@@ -0,0 +1,15 @@
+package junit.bowler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BezierEditorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
