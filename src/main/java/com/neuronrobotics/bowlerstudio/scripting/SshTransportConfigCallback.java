9ca777b9d1f8cd3a176f4aa11e18f2e6b978330b
Kevin Harrington <mad.hephaestus@gmail.com>
2023-04-14 18:14:27 -0400

    less change

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/SshTransportConfigCallback.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/SshTransportConfigCallback.java
@@ -5,11 +5,11 @@ import org.eclipse.jgit.transport.SshTransport;
 import org.eclipse.jgit.transport.Transport;
 
 public class SshTransportConfigCallback implements TransportConfigCallback {
-    org.eclipse.jgit.transport.sshd.SshdSessionFactory factory = new org.eclipse.jgit.transport.sshd.SshdSessionFactory();
+    org.eclipse.jgit.transport.sshd.SshdSessionFactory sshSessionFactory = new org.eclipse.jgit.transport.sshd.SshdSessionFactory();
 
     @Override
     public void configure(Transport transport) {
         SshTransport sshTransport = (SshTransport) transport;
-		sshTransport.setSshSessionFactory(factory);
+		sshTransport.setSshSessionFactory(sshSessionFactory);
     }
 }
