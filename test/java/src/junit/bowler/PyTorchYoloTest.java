ff4a0de2afae64f1f26264121365c87439a4cde2
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-19 15:27:19 -0400

    adding ML deps

--- test/java/src/junit/bowler/PyTorchYoloTest.java
+++ test/java/src/junit/bowler/PyTorchYoloTest.java
@@ -4,13 +4,19 @@ import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
 import ai.djl.Application;
 import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications.Classification;
+import ai.djl.modality.cv.output.BoundingBox;
 import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.modality.cv.output.DetectedObjects.DetectedObject;
+import ai.djl.modality.cv.output.Point;
+import ai.djl.modality.cv.output.Rectangle;
 import ai.djl.modality.cv.util.BufferedImageUtils;
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelNotFoundException;
@@ -35,6 +41,15 @@ public class PyTorchYoloTest {
 
 		Predictor<BufferedImage, DetectedObjects> predictor = model.newPredictor();
 		DetectedObjects detection = predictor.predict(img);
+		List<DetectedObject> items = detection.items();
+		for (int i = 0; i < items.size(); i++) {
+			DetectedObject c = items.get(i);
+			BoundingBox cGetBoundingBox = c.getBoundingBox();
+			Point topLeft = cGetBoundingBox.getPoint();
+			Rectangle rect = cGetBoundingBox.getBounds();
+			System.out.println(c);
+			System.out.println("Name: "+c.getClassName() +" probability "+c.getProbability()+" center x "+topLeft.getX()+" center y "+topLeft.getY()+" rect h"+rect.getHeight()+" rect w"+rect.getWidth() );
+		}
 		System.out.println(detection);
 	}
 
