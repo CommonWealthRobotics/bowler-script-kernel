854b658cfa374e94982a5e96ba1555eb5ac37e03
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-19 22:27:35 -0400

    multi test

--- test/java/src/junit/bowler/PyTorchResNetTest.java
+++ test/java/src/junit/bowler/PyTorchResNetTest.java
@@ -20,9 +20,10 @@ import ai.djl.modality.cv.output.DetectedObjects;
 
 public class PyTorchResNetTest {
 	String imageUrl = "https://avatars.githubusercontent.com/u/1254726?v=4";
-	String image2URL="https://images.squarespace-cdn.com/content/v1/51f533d1e4b0de43ba620290/f2120e79-d8dc-4a34-aa8c-03dc5fe5f0df/LMheadshot.png?format=300w";
-	String image3URL="https://static1.squarespace.com/static/56ffcc3901dbae8fc9b21603/t/572f5e92cf80a12c4b5c66fe/1462722410298/?format=1500w";
-	String notme="https://images.squarespace-cdn.com/content/v1/51f533d1e4b0de43ba620290/1472046872933-F3PCYXEK429ZJHDZSWLC/image-asset.jpeg?format=300w";
+	String image2URL = "https://images.squarespace-cdn.com/content/v1/51f533d1e4b0de43ba620290/f2120e79-d8dc-4a34-aa8c-03dc5fe5f0df/LMheadshot.png?format=300w";
+	String image3URL = "https://static1.squarespace.com/static/56ffcc3901dbae8fc9b21603/t/572f5e92cf80a12c4b5c66fe/1462722410298/?format=1500w";
+	String notme = "https://images.squarespace-cdn.com/content/v1/51f533d1e4b0de43ba620290/1472046872933-F3PCYXEK429ZJHDZSWLC/image-asset.jpeg?format=300w";
+
 	@Test
 	public void testResNet() throws Exception {
 
@@ -32,13 +33,15 @@ public class PyTorchResNetTest {
 	public void testUltraNet() throws Exception {
 		BufferedImage bimg = ImageIO.read(new URL(imageUrl));
 
-		Image img = ImageFactory.getInstance().fromImage(bimg);
-
 		Predictor<Image, DetectedObjects> predictor = PredictorFactory
 				.imageContentsFactory(ImagePredictorType.ultranet);
 
-		DetectedObjects objects = predictor.predict(img);
-		saveBoundingBoxImage(img, objects, "ultranet");
+		for (int i = 0; i < 3; i++) {
+			Image img = ImageFactory.getInstance().fromImage(bimg);
+
+			DetectedObjects objects = predictor.predict(img);
+			saveBoundingBoxImage(img, objects, "ultranet");
+		}
 	}
 
 	private static void saveBoundingBoxImage(Image img, DetectedObjects detection, String type) throws Exception {
@@ -56,11 +59,13 @@ public class PyTorchResNetTest {
 	public void testYolo() throws Exception {
 
 		Predictor<Image, DetectedObjects> predictor = PredictorFactory.imageContentsFactory(ImagePredictorType.yolov5);
-		Image input = ImageFactory.getInstance()
-				.fromUrl("https://github.com/ultralytics/yolov5/raw/master/data/images/bus.jpg");
-		DetectedObjects objects = predictor.predict(input);
-		saveBoundingBoxImage(input, objects, "yolov5");
+		for (int i = 0; i < 3; i++) {
+			Image input = ImageFactory.getInstance()
+					.fromUrl("https://github.com/ultralytics/yolov5/raw/master/data/images/bus.jpg");
 
+			DetectedObjects objects = predictor.predict(input);
+			saveBoundingBoxImage(input, objects, "yolov5");
+		}
 	}
 
 	@Test
@@ -68,22 +73,20 @@ public class PyTorchResNetTest {
 		BufferedImage img = ImageIO.read(new URL(imageUrl));
 		BufferedImage img2 = ImageIO.read(new URL(image2URL));
 		BufferedImage img3 = ImageIO.read(new URL(image3URL));
-		
+
 		Predictor<Image, float[]> predictor = PredictorFactory.faceFeatureFactory();
 		float[] back = predictor.predict(ImageFactory.getInstance().fromImage(img));
 		float[] back2 = predictor.predict(ImageFactory.getInstance().fromImage(img2));
-		float[] back3= predictor.predict(ImageFactory.getInstance().fromImage(img3));
-		float[] back4= predictor.predict(ImageFactory.getInstance().fromImage(ImageIO.read(new URL(notme))));
-		
-		System.out.println(" Comprair 1 to 2 "+PredictorFactory.calculSimilarFaceFeature(back, back2));
-		System.out.println(" Comprair 1 to 3 "+PredictorFactory.calculSimilarFaceFeature(back, back3));
-		System.out.println(" Comprair 2 to 3 "+PredictorFactory.calculSimilarFaceFeature(back2, back3));
-		System.out.println(" Comprair 1 to 4 "+PredictorFactory.calculSimilarFaceFeature(back, back4));
+		float[] back3 = predictor.predict(ImageFactory.getInstance().fromImage(img3));
+		float[] back4 = predictor.predict(ImageFactory.getInstance().fromImage(ImageIO.read(new URL(notme))));
+
+		System.out.println(" Comprair 1 to 2 " + PredictorFactory.calculSimilarFaceFeature(back, back2));
+		System.out.println(" Comprair 1 to 3 " + PredictorFactory.calculSimilarFaceFeature(back, back3));
+		System.out.println(" Comprair 2 to 3 " + PredictorFactory.calculSimilarFaceFeature(back2, back3));
+		System.out.println(" Comprair 1 to 4 " + PredictorFactory.calculSimilarFaceFeature(back, back4));
 
 		System.out.println(Arrays.toString(back));
 
 	}
-
-
 
 }
