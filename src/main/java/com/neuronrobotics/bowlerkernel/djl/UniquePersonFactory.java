804c2dd35f8f13cfa68bf50793a7dc10bfdecc16
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-05 22:55:10 -0400

    unique persons database

--- src/main/java/com/neuronrobotics/bowlerkernel/djl/UniquePersonFactory.java
+++ src/main/java/com/neuronrobotics/bowlerkernel/djl/UniquePersonFactory.java
@@ -1,6 +1,8 @@
 package com.neuronrobotics.bowlerkernel.djl;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -35,7 +37,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
+import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 
 public class UniquePersonFactory extends NonBowlerDevice {
 	private File database;
@@ -46,16 +51,17 @@ public class UniquePersonFactory extends NonBowlerDevice {
 	private ArrayList<UniquePerson> shortTermMemory = new ArrayList<>();;
 	Predictor<Image, float[]> features;
 
-	private static double confidence = 0.88;
+	private static double confidence = 0.90;
 	private static long timeout = 30000;
 	private static long countPeople = 1;
-	private static int numberOfTrainingHashes = 50;
+	private static int numberOfTrainingHashes = 20;
 	private Thread processor;
 	private boolean run = true;
 	private HashMap<BufferedImage, Point> factoryFromImageTMp = null;
 	private HashMap<UniquePerson, Point> currentPersons = null;
 	private ImageFactory factory;
 	private VBox workingMemory = new VBox();
+	private boolean processFlag = false;
 
 	private class UniquePersonUI {
 		HBox box = new HBox();
@@ -175,22 +181,43 @@ public class UniquePersonFactory extends NonBowlerDevice {
 		return run;
 	}
 
+	public void addFace(Mat matrix, Rect crop, ai.djl.modality.cv.output.Point nose) {
+		if (factoryFromImageTMp == null) {
+			factoryFromImageTMp = new HashMap<BufferedImage, Point>();
+		}
+		try {
+			Mat tmpImg = new Mat(matrix, crop);
+			Mat image_roi = new Mat();
+			// Converting the image to grey scale
+			Imgproc.cvtColor(tmpImg, image_roi, Imgproc.COLOR_RGB2GRAY);
+			BufferedImage image = new BufferedImage(crop.width,
+					crop.height, BufferedImage.TYPE_BYTE_GRAY);
+			WritableRaster raster = image.getRaster();
+			DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+			byte[] data = dataBuffer.getData();
+			image_roi.get(0, 0, data);
+			factoryFromImageTMp.put(image, new Point(nose.getX(),nose.getY()));
+		} catch (Throwable tr) {
+			tr.printStackTrace();
+		}
+	}
+
 	private void processBlocking() {
 		JniUtils.setGraphExecutorOptimize(false);
 		while (run) {
-			if (factoryFromImageTMp == null) {
+			if (!isProcessFlag()) {
 				try {
 					Thread.sleep(16);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 					break;
 				}
 				continue;
 			}
-			HashMap<BufferedImage, Point> factoryFromImage = factoryFromImageTMp;
-			factoryFromImageTMp = null;
+			processFlag = false;
 			try {
+				HashMap<BufferedImage, Point> local = new HashMap<>();
+				local.putAll(factoryFromImageTMp);
+				factoryFromImageTMp = null;
 				for (UniquePerson up : shortTermMemory) {
 					if (up.timesSeen > numberOfTrainingHashes) {
 						if (!longTermMemory.contains(up)) {
@@ -200,14 +227,23 @@ public class UniquePersonFactory extends NonBowlerDevice {
 						}
 					}
 				}
-				HashMap<BufferedImage, Point> local = new HashMap<>();
-				local.putAll(factoryFromImage);
+				
 				HashMap<UniquePerson, org.opencv.core.Point> tmpPersons = new HashMap<>();
 				for (BufferedImage imgBuff : local.keySet()) {
 					ai.djl.modality.cv.Image cmp = factory.fromImage(imgBuff);
 					Point point = local.get(imgBuff);
 					// println "Processing new image "
-					float[] id = features.predict(cmp);
+					int minSize = 70;
+					if (imgBuff.getHeight() < minSize || imgBuff.getWidth() < minSize)
+						continue;
+					float[] id;
+					try {
+						id = features.predict(cmp);
+					} catch (Throwable ex) {
+						System.out.println("Image failed h=" + imgBuff.getHeight() + " w=" + imgBuff.getWidth());
+						ex.printStackTrace();
+						continue;
+					}
 					boolean found = false;
 					ArrayList<UniquePerson> duplicates = new ArrayList<UniquePerson>();
 
@@ -229,8 +265,9 @@ public class UniquePersonFactory extends NonBowlerDevice {
 								found = true;
 								if (p.timesSeen > 2)
 									tmpPersons.put(p, point);
-								if (p.timesSeen > 3 && uiElelments.get(p) == null) {
-									UniquePersonUI UI = getUI(p);
+								UniquePersonUI UI = getUI(p);
+
+								if (p.timesSeen > 3 && !workingMemory.getChildren().contains(UI.box)) {
 									// on the third seen, display
 									WritableImage tmpImg = SwingFXUtils.toFXImage(imgBuff, null);
 									UI.box.getChildren().addAll(new ImageView(tmpImg));
@@ -243,24 +280,26 @@ public class UniquePersonFactory extends NonBowlerDevice {
 								}
 								p.time = System.currentTimeMillis();
 								// if(result<(confidence+0.01))
-								if (p.features.size() < numberOfTrainingHashes) {
-									p.features.add(id);
-									int percent = (int) (((double) p.features.size())
-											/ ((double) numberOfTrainingHashes) * 100);
-									// println "Trained "+percent;
-									UniquePersonUI UI = getUI(p);
+								// if (p.features.size() < numberOfTrainingHashes) {
+								p.features.add(id);
+								int percent = (int) (((double) p.features.size()) / ((double) numberOfTrainingHashes)
+										* 100);
+								if (percent > 100)
+									percent = 100;
+								double perc = percent;
+								// println "Trained "+percent;
+								Platform.runLater(() -> {
+									UI.percent.setText(" : Trained " + perc + "%");
+								});
+								p.confidenceTarget = confidence;
+								if (p.features.size() == numberOfTrainingHashes) {
+									// println " Trained "+p.name;
 									Platform.runLater(() -> {
-										UI.percent.setText(" : Trained " + percent + "%");
+										UI.box.getChildren().addAll(new Label(" Done! "));
 									});
-									p.confidenceTarget = confidence;
-									if (p.features.size() == numberOfTrainingHashes) {
-										// println " Trained "+p.name;
-										Platform.runLater(() -> {
-											UI.box.getChildren().addAll(new Label(" Done! "));
-										});
 
-									}
 								}
+								// }
 							}
 						}
 					}
@@ -273,13 +312,13 @@ public class UniquePersonFactory extends NonBowlerDevice {
 					for (UniquePerson p : duplicates) {
 						shortTermMemory.remove(p);
 						UniquePersonUI UI = getUI(p);
-
+						uiElelments.remove(p);
 						Platform.runLater(() -> {
 							workingMemory.getChildren().remove(UI.box);
 						});
 						// println "Removing "+p.name
 					}
-					
+
 					if (found == false) {
 						resetHash();
 						countPeople++;
@@ -307,13 +346,6 @@ public class UniquePersonFactory extends NonBowlerDevice {
 	public ArrayList<String> getNamespacesImp() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	/**
-	 * @param factoryFromImage the factoryFromImage to set
-	 */
-	public void setFactoryFromImage(HashMap<BufferedImage, Point> factoryFromImage) {
-		this.factoryFromImageTMp = factoryFromImage;
 	}
 
 	/**
@@ -349,18 +381,18 @@ public class UniquePersonFactory extends NonBowlerDevice {
 	public File getDatabase() {
 		return database;
 	}
-	
+
 	private void resetHash() {
-		countPeople=0;
+		countPeople = 0;
 		for (UniquePerson u : longTermMemory) {
 			if (u.UUID > countPeople)
 				countPeople = u.UUID;
-			
+
 		}
 		for (UniquePerson u : shortTermMemory) {
 			if (u.UUID > countPeople)
 				countPeople = u.UUID;
-			
+
 		}
 	}
 
@@ -391,5 +423,19 @@ public class UniquePersonFactory extends NonBowlerDevice {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	/**
+	 * @return the processFlag
+	 */
+	public boolean isProcessFlag() {
+		return processFlag;
+	}
+
+	/**
+	 * @param processFlag the processFlag to set
+	 */
+	public void setProcessFlag() {
+		this.processFlag = true;
 	}
 }
