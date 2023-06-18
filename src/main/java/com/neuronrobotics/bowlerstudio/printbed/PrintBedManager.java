aa65b4ed962dce079f2e742b5ea29a73ac9f6892
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-18 18:28:29 -0400

    move print bed to top

--- src/main/java/com/neuronrobotics/bowlerstudio/printbed/PrintBedManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/printbed/PrintBedManager.java
@@ -29,7 +29,7 @@ public class PrintBedManager {
 	Type type = new TypeToken<UserManagedPrintBedData>() {
 	}.getType();
 	Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-	String file = "manufacturing/printbed.json";
+	String file = "printbed.json";
 	List<Color> colors = Arrays.asList(Color.WHITE,Color.GREY,Color.BLUE,Color.TAN);
 	private String url;
 	HashMap<Integer ,CSG>  bedReps = new HashMap<Integer, CSG>();
