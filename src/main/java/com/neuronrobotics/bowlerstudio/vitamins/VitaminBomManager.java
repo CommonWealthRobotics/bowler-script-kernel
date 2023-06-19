aed22bff4b1e833cc6a3bc9340d81205907d2b40
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-19 14:38:54 -0400

    no synchronized or threading

--- src/main/java/com/neuronrobotics/bowlerstudio/vitamins/VitaminBomManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/vitamins/VitaminBomManager.java
@@ -22,9 +22,9 @@ import javafx.scene.paint.Color;
 
 public class VitaminBomManager {
 	public static final String MANUFACTURING_BOM_BASE = "manufacturing/bom";
-	public static final String MANUFACTURING_BOM_JSON = MANUFACTURING_BOM_BASE+".json";
-	public static final String MANUFACTURING_BOM_CSV = MANUFACTURING_BOM_BASE+".csv";
-
+	public static final String MANUFACTURING_BOM_JSON = MANUFACTURING_BOM_BASE + ".json";
+	public static final String MANUFACTURING_BOM_CSV = MANUFACTURING_BOM_BASE + ".csv";
+	private static boolean saving = false;
 	private class VitaminElement {
 		String name;
 		String type;
@@ -37,11 +37,11 @@ public class VitaminBomManager {
 	Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
 	private HashMap<String, ArrayList<VitaminElement>> database = new HashMap<String, ArrayList<VitaminElement>>();
 	private String baseURL;
-	
+
 	public VitaminBomManager(String url) throws IOException {
 		baseURL = url;
 		File baseWorkspaceFile = ScriptingEngine.getRepositoryCloneDirectory(baseURL);
-		File bom = new File(baseWorkspaceFile.getAbsolutePath() + "/"+MANUFACTURING_BOM_JSON);
+		File bom = new File(baseWorkspaceFile.getAbsolutePath() + "/" + MANUFACTURING_BOM_JSON);
 		if (!bom.exists()) {
 			if (!bom.getParentFile().exists()) {
 				bom.getParentFile().mkdir();
@@ -54,7 +54,7 @@ public class VitaminBomManager {
 			}
 			save();
 		} else {
-			String source ;
+			String source;
 			byte[] bytes;
 			try {
 				bytes = Files.readAllBytes(bom.toPath());
@@ -68,22 +68,23 @@ public class VitaminBomManager {
 
 	public void set(String name, String type, String size, TransformNR location) {
 		VitaminElement newElement = getElement(name);
-		boolean toAdd=false;
-		if(newElement==null) {
+		boolean toAdd = false;
+		if (newElement == null) {
 			newElement = new VitaminElement();
 			newElement.name = name;
-			toAdd=true;
+			toAdd = true;
 		}
 		newElement.pose = location;
 		newElement.size = size;
 		newElement.type = type;
-		String key = type+":"+size;
-		if(database.get(key)==null) {
-			database.put(key,new ArrayList<VitaminBomManager.VitaminElement>());
-		}
-		if(toAdd)
-			database.get(key).add(newElement);
-		
+		String key = type + ":" + size;
+		//synchronized (database) {
+			if (database.get(key) == null) {
+				database.put(key, new ArrayList<VitaminBomManager.VitaminElement>());
+			}
+			if (toAdd)
+				database.get(key).add(newElement);
+		//}
 		save();
 		// newElement.url=(String) getConfiguration(name).get("source");
 	}
@@ -95,7 +96,9 @@ public class VitaminBomManager {
 
 		try {
 			CSG transformed = Vitamins.get(e.type, e.size).transformed(TransformFactory.nrToCSG(e.pose));
-			transformed.setManufacturing(incominng->{return null;});
+			transformed.setManufacturing(incominng -> {
+				return null;
+			});
 			transformed.setColor(Color.SILVER);
 			return transformed;
 
@@ -105,17 +108,17 @@ public class VitaminBomManager {
 		}
 		return null;
 	}
-	
+
 	public TransformNR getCoMLocation(String name) {
 		VitaminElement e = getElement(name);
-		double x=(double) getConfiguration(name).get("massCentroidX");
-		double y=(double) getConfiguration(name).get("massCentroidY");
+		double x = (double) getConfiguration(name).get("massCentroidX");
+		double y = (double) getConfiguration(name).get("massCentroidY");
 
-		double z=(double) getConfiguration(name).get("massCentroidZ");
+		double z = (double) getConfiguration(name).get("massCentroidZ");
 
 		return e.pose.copy().translateX(x).translateY(y).translateZ(z);
 	}
-	
+
 	public double getMassKg(String name) {
 		return (double) getConfiguration(name).get("massKg");
 	}
@@ -129,34 +132,46 @@ public class VitaminBomManager {
 	}
 
 	private VitaminElement getElement(String name) {
-		for (String testName : database.keySet()) {
-			ArrayList<VitaminElement> list=database.get(testName);
-			for (VitaminElement el : list) {
-				if (el.name.contentEquals(name))
-					return el;
+		//synchronized (database) {
+			for (String testName : database.keySet()) {
+				ArrayList<VitaminElement> list = database.get(testName);
+				for (VitaminElement el : list) {
+					if (el.name.contentEquals(name))
+						return el;
+				}
 			}
-		}
+		//}
 		return null;
 	}
 
 	public void clear() {
-		database.clear();
+		//synchronized (database) {
+			database.clear();
+		//}
 	}
-	private synchronized void saveLocal() {
-		String content = gson.toJson(database);
-		//String[] source = base.getGitSelfSource();
-		String csv ="name,qty,source\n";
-		for(Object key:database.keySet().toArray()) {
-			VitaminElement e  =database.get(key).get(0);
-			String size = database.get(key).size()+"";
-			String URL = (String) getConfiguration(e.name).get("source");
-			csv+=key+","+size+","+URL+"\n";
-		}
+
+	private void saveLocal() {
+		saving=true;
+		String csv = "name,qty,source\n";
+		String content = null;
+		
+			content = gson.toJson(database);
+			// String[] source = base.getGitSelfSource();
+
+			for (String key : database.keySet()) {
+				VitaminElement e = database.get(key).get(0);
+				String size = database.get(key).size() + "";
+				String URL = (String) getConfiguration(e.name).get("source");
+				csv += key + "," + size + "," + URL + "\n";
+			}
+		
+		
 		try {
-			String current = ScriptingEngine.codeFromGit(baseURL,MANUFACTURING_BOM_CSV)[0];
-			String currentJ = ScriptingEngine.codeFromGit(baseURL,MANUFACTURING_BOM_JSON)[0];
-			if(current.contentEquals(csv) && currentJ.contentEquals(content)) {
+			String current = ScriptingEngine.codeFromGit(baseURL, MANUFACTURING_BOM_CSV)[0];
+			String currentJ = ScriptingEngine.codeFromGit(baseURL, MANUFACTURING_BOM_JSON)[0];
+			if (current.contentEquals(csv) && currentJ.contentEquals(content)) {
 				System.out.println("No update, BoM current");
+				saving=false;
 				return;
 			}
 		} catch (Exception e1) {
@@ -164,19 +179,20 @@ public class VitaminBomManager {
 			e1.printStackTrace();
 		}
 		try {
-			ScriptingEngine.commit(baseURL, ScriptingEngine.getBranch(baseURL),MANUFACTURING_BOM_JSON, content,
+			ScriptingEngine.commit(baseURL, ScriptingEngine.getBranch(baseURL), MANUFACTURING_BOM_JSON, content,
 					"Save Bill Of Material", true);
-			ScriptingEngine.commit(baseURL, ScriptingEngine.getBranch(baseURL),MANUFACTURING_BOM_CSV, csv,
+			ScriptingEngine.commit(baseURL, ScriptingEngine.getBranch(baseURL), MANUFACTURING_BOM_CSV, csv,
 					"Save Bill Of Material", true);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		saving=false;
+		
 	}
+
 	public void save() {
-		new Thread(()->{
-			saveLocal();
-		}).start();
+		saveLocal();
 	}
 
 }
