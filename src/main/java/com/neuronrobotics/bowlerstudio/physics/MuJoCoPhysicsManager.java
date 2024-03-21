26788d3d63b07816142d0782ed46e44552cb9f54
Kevin Harrington <mad.hephaestus@gmail.com>
2024-03-21 18:36:23 -0400

    Working physics generation for wheels and feet

--- src/main/java/com/neuronrobotics/bowlerstudio/physics/MuJoCoPhysicsManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/physics/MuJoCoPhysicsManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.vecmath.Color3b;
 import javax.xml.bind.JAXBException;
@@ -27,6 +28,7 @@ import org.mujoco.xml.body.JointType;
 
 import com.neuronrobotics.bowlerstudio.BowlerKernel;
 import com.neuronrobotics.bowlerstudio.creature.MobileBaseCadManager;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import com.neuronrobotics.sdk.addons.kinematics.AbstractLink;
 import com.neuronrobotics.sdk.addons.kinematics.DHLink;
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
@@ -41,6 +43,7 @@ import com.neuronrobotics.sdk.util.ThreadUtil;
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.Cube;
 import eu.mihosoft.vrl.v3d.Polygon;
+import eu.mihosoft.vrl.v3d.RoundedCylinder;
 import eu.mihosoft.vrl.v3d.Transform;
 import eu.mihosoft.vrl.v3d.Vector3d;
 import javafx.scene.paint.Color;
@@ -77,6 +80,7 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 	public  CSG floor = new Cube(4000,4000,1000).toCSG().toZMax().setName("floor").setColor(Color.PINK);
 	public  long timeSinceUIUpdate = 0;
 	public Mujoco.Contact.Builder<? > contacts;
+	public Builder<?> actuators;
 	private IntegratorType integratorType=IntegratorType.RK_4;
 	public HashMap<AbstractLink, Double> gearRatios = new HashMap<>();
 	public long currentTimeMillis() {
@@ -213,9 +217,9 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 		initializeModel(name);
 		if (bases != null)
 			if (bases.size() > 0) {
-				Builder<?> actuators = builder.addActuator();
+				actuators = builder.addActuator();
 				for (MobileBase cat : bases) {
-					loadBase(cat, actuators);
+					loadBase(cat);
 				}
 			}
 		if (freeObjects != null)
@@ -387,17 +391,18 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 	
 		return cadMan.computeLowestPoint().z;
 	}
-	public void loadBase(MobileBase cat, Builder<?> actuators) throws IOException{
-		loadBase(cat,actuators,null);
+	public void loadBase(MobileBase cat) throws IOException{
+		loadBase(cat,null,new TransformNR());
 	}
 
-	public void loadBase(MobileBase cat, Builder<?> actuators, org.mujoco.xml.BodyarchType.Builder<?> inbody) throws IOException {
+	public void loadBase(MobileBase cat, org.mujoco.xml.BodyarchType.Builder<?> linkBody2, TransformNR offsetGlobal) throws IOException {
 		if(contacts==null)
 			contacts= builder.addContact();
 		boolean freeBase=cat.getSteerable().size()>0||
 				cat.getDrivable().size()>0||
 				cat.getLegs().size()>0;
-		
+		double KgtoMujocoMass =1.0;
+				
 		//println "\n\nLowest point "+lowestPoint+" \n\n";
 		String bodyName = getMujocoName(cat);
 		MobileBaseCadManager cadMan = MobileBaseCadManager.get(cat);
@@ -407,14 +412,34 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 		int bodyParts=0;
 
 		org.mujoco.xml.BodyarchType.Builder<?> addBody;
-		if(inbody==null) {
+		offsetGlobal=offsetGlobal.times(cat.getRobotToFiducialTransform());
+		if(linkBody2==null) {
 			addBody= addWorldbody.addBody();
+			String centerString = offsetGlobal.getX()/1000.0+" "+
+			offsetGlobal.getY()/1000.0+" "+
+			lowestPoint;
+			String quat =offsetGlobal.getRotation().getRotationMatrix2QuaturnionW()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionX()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionY()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionZ();
+			addBody.withPos(centerString)
+					.withQuat(quat);// move the base to 1mm above the z=0 surface
+			
 		}else {
-			addBody=inbody.addBody();
+			String centerString = offsetGlobal.getX()/1000.0+" "+
+			offsetGlobal.getY()/1000.0+" "+
+			offsetGlobal.getZ()/1000.0+" ";
+			String quat =offsetGlobal.getRotation().getRotationMatrix2QuaturnionW()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionX()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionY()+" "+
+						offsetGlobal.getRotation().getRotationMatrix2QuaturnionZ();
+			addBody=linkBody2.addBody();
+			addBody.withPos(centerString)
+					.withQuat(quat);// move the base to 1mm above the z=0 surface
+			
 		}
 		addBody.withName(bodyName);
 		
-		addBody.withPos("0 0 "+lowestPoint);// move the base to 1mm above the z=0 surface
 		
 		if(freeBase) {
 			addBody.addFreejoint();
@@ -425,6 +450,7 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 	//					.withDiaginertia("1 1 1" );
 		ArrayList<CSG> arrayList = cadMan.getAllCad();
 		HashMap<CSG,TransformNR > baseParts=new HashMap<>();
+		HashMap<CSG,TransformNR > limbBase=new HashMap<>();
 		for (int i = 0; i < arrayList.size(); i++) {
 			CSG part = arrayList.get(i);
 			if(!checkForPhysics(part))
@@ -433,33 +459,39 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 			boolean foundPart = false;
 			for(DHParameterKinematics k: cat.getAllDHChains()) {
 				if(k.getRootListener() == part.getManipulator()) {
-					TransformNR kfed = k.getRobotToFiducialTransform();
-					center=center.times(kfed);
+					TransformNR fiducial =k.getRobotToFiducialTransform();
+					TransformNR kfed = fiducial;
+					//center=kfed.times(center);
+					//center=kfed.times(center.inverse());
+					center = new TransformNR();
+					limbBase.put(part, kfed);
 					foundPart=true;
 					break;
 				}
 			}
-			if(!foundPart)
+			if(!foundPart) {
 				if(part.getManipulator()!=cat.getRootListener())
 					continue;
-			baseParts.put(part,center.inverse());
+				limbBase.put(part, new TransformNR());
+			}
+			baseParts.put(part,center.copy());
 
 		}
-		
 		for(CSG part:baseParts.keySet()) {
 			TransformNR center = baseParts.get(part);
+			TransformNR offset = limbBase.get(part);
 			double mass = cat.getMassKg()/baseParts.size();
 			bodyParts++;
 			String nameOfCSG = bodyName+"_CSG_"+bodyParts;
 			
-			CSG transformed = part.transformed(TransformFactory.nrToCSG(center));
+			CSG transformed = part.transformed(TransformFactory.nrToCSG(center.inverse().times(offset)));
 			CSG hull ;
 			try {
 					hull=transformed.hull();
 			}catch(Exception ex) {
 				hull=transformed;
 			}
-			transformed=part.clone();
+			transformed=part.transformed(TransformFactory.nrToCSG(offset));
 			transformed.setManipulator(new Affine());
 			putCSGInAssets(nameOfCSG, hull,true);
 			org.mujoco.xml.body.GeomType.Builder<?> geom = addBody.addGeom();
@@ -476,7 +508,7 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 			
 			geom.withPos(centerString)
 				.withQuat(quat)
-				.withMass(BigDecimal.valueOf(mass));
+				.withMass(BigDecimal.valueOf(mass*KgtoMujocoMass));
 		}
 		
 		for(DHParameterKinematics l:cat.getAllDHChains()) {
@@ -515,13 +547,14 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 
 			for(int i=0;i<k.getNumberOfLinks();i++) {
 				AbstractLink link = k.getAbstractLink(i);
+				
 				LinkConfiguration conf = link.getLinkConfiguration();
 				ArrayList<CSG>  parts=cadMan.getLinktoCadMap().get(conf);
 				if(checkLinkPhysics(cadMan,conf))
-					linkBody=loadLink(k,i,parts,linkBody,actuators,linkToBulder);
+					linkBody=loadLink(cat,k,i,parts,linkBody,linkToBulder);
 				MobileBase follower = k.getFollowerMobileBase(link);
 				if(follower!=null) {
-					loadBase(follower, actuators, linkBody);
+					loadBase(follower, linkBody,k.getDHStep(i));
 				}
 			}
 			for(String affineNameMapGet:geomToCSGMap.keySet()) {
@@ -534,7 +567,7 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 				}
 				for(org.mujoco.xml.body.GeomType.Builder<?>geom:geoms) {
 					//println "Mass of "+affineNameMapGet+" is "+mass
-					geom.withMass(BigDecimal.valueOf(mass));
+					geom.withMass(BigDecimal.valueOf(mass*KgtoMujocoMass));
 				}
 			}
 			
@@ -542,139 +575,170 @@ public class MuJoCoPhysicsManager implements IMujocoController,ITimeProvider {
 	}
 	
 	public org.mujoco.xml.BodyarchType.Builder<?> loadLink(
-			DHParameterKinematics l,
-			int index,ArrayList<CSG>  cad,
-			org.mujoco.xml.BodyarchType.Builder<?> addBody,
-			Builder<?> actuators,
-			HashMap<AbstractLink,org.mujoco.xml.BodyarchType.Builder<?>> linkToBulderMap) {
-		AbstractLink link = l.getAbstractLink(index);
-		LinkConfiguration conf = link.getLinkConfiguration();
+		MobileBase cat,
+		DHParameterKinematics l,
+		int index,ArrayList<CSG>  cad,
+		org.mujoco.xml.BodyarchType.Builder<?> addBody,
+		HashMap<AbstractLink,org.mujoco.xml.BodyarchType.Builder<?>> linkToBulderMap) {
+	AbstractLink link = l.getAbstractLink(index);
+	LinkConfiguration conf = link.getLinkConfiguration();
 
-		String name=null;
-		for(String s:mapNameToLink.keySet()) {
-			if(mapNameToLink.get(s)==link) {
-				name=s;
-				break;
-			}
+	String name=null;
+	for(String s:mapNameToLink.keySet()) {
+		if(mapNameToLink.get(s)==link) {
+			name=s;
+			break;
 		}
-		if(name ==null)
-			throw new RuntimeException("Link name missing from the map!");
-
-		TransformNR location;
-		if(index==0) {
-			location=l.getRobotToFiducialTransform().copy();
-		}else {
-			location=new TransformNR(l.getDhLink(index-1).DhStep(0));
-		}
-		TransformNR local = new TransformNR(l.getDhLink(index).DhStep(0));
-		Transform step = TransformFactory.nrToCSG(local);
-		double x = location.getX()/1000.0;
-
-		double y = location.getY()/1000.0;
-
-		double z = location.getZ()/1000.0;
-		String quat =location.getRotation().getRotationMatrix2QuaturnionW()+" "+
-				location.getRotation().getRotationMatrix2QuaturnionX()+" "+
-				location.getRotation().getRotationMatrix2QuaturnionY()+" "+
-				location.getRotation().getRotationMatrix2QuaturnionZ();
-		TransformNR center = conf.getCenterOfMassFromCentroid();
-		String centerString = center.getX()/1000.0+" "+
-				center.getY()/1000.0+" "+
-				center.getZ()/1000.0+" ";
-		org.mujoco.xml.BodyarchType.Builder<?> linkBody = addBody.addBody()
-				.withName(name)
-				.withPos(x+" "+y+" "+z)
-				.withQuat(quat);
-		//		linkBody.addInertial()
-		//				.withMass(conf.getMassKg())
-		//				.withPos(centerString)
-		//				.withDiaginertia("1 1 1" )
-		linkToBulderMap.put(link, linkBody);
-
-		double gear =260;
-		gearRatios.put(link,1.0d*gear);
-		double upper = Math.toRadians(link.getMaxEngineeringUnits());
-		double lower = Math.toRadians(link.getMinEngineeringUnits());
-		String range=lower+" "+upper;
-		String ctrlRange=(lower*gear)+" "+(upper*gear);
-		double rangeVal=upper-lower;
-		JointType.Builder<?> jointBuilder = linkBody.addJoint();
-		String axisJoint ="0 0 1";
-		///position=0;
-		jointBuilder
-				.withPos("0 0 0")// the kinematic center
-				.withAxis(axisJoint) // rotate about the z axis per dh convention
-				.withRange(ctrlRange) // engineering units range
-				.withRef(BigDecimal.valueOf(0)) // set the reference position on loading as the links 0 degrees value
-				.withType(JointtypeType.HINGE) // hinge type
-				.withFrictionloss(BigDecimal.valueOf(0.0001))// experementally determined
-				//.withLimited(true)
-				//.withDamping(BigDecimal.valueOf(0.00001))
-				//.withStiffness(BigDecimal.valueOf(1))
-				.withName(name)
-		;
-		if (!conf.isPassive()) {
-			actuators.addPosition()// A position controller to model a servo
-					.withKp(BigDecimal.valueOf(0.00004)) // experementally determined value
-					//.withForcelimited(true)
-					//.withForcerange(range)
-					.withCtrlrange(ctrlRange)
-					.withName(name)// name the motor
-					.withJoint(name)// which joint this motor powers
-					.withGear(""+gear) // gear ratio between virtual motor and output
-					.withKv(BigDecimal.valueOf(0.0000008)); // damping term experementally determenied
-					//.withInheritrange(BigDecimal.valueOf(rangeVal));// sets the range of the control signal to match the limits
-		}
-		for (int i = 0; i < cad.size(); i++) {
-			CSG part = cad.get(i);
-			if(!checkForPhysics(part))
-				continue;
-			Affine cGetManipulator = part.getManipulator();
-			if(cGetManipulator!=null) {
-				String affineNameMapGet = affineNameMap.get(cGetManipulator);
-				if(affineNameMapGet!=null) {
-					DHParameterKinematics k = l;
-					
-					AbstractLink myLink = mapNameToLink.get(affineNameMapGet);
-					double myposition = link.getCurrentEngineeringUnits();
-					TransformNR myStep = new TransformNR(
-							k.getDhLink(myLink).DhStep(0)
-							);
-					CSG transformed = part
-							.transformed(
-							TransformFactory
-							.nrToCSG(
-							myStep
-							))
-							;
-					CSG hull = transformed.hull();
-					//					if(myLink!=link)
-					//						hull = part.hull();
-					transformed.setManipulator(new Affine());
-					String geomname=name+" "+i;
-
-					try {
-						putCSGInAssets(geomname, hull,true);
-						org.mujoco.xml.body.GeomType.Builder<?> geom = linkToBulderMap.get(myLink).addGeom();
-
-						ArrayList<CSG> parts = getMapNameToCSGParts(affineNameMapGet);
-						if(geomToCSGMap.get(affineNameMapGet)==null) {
-							geomToCSGMap.put(affineNameMapGet, new ArrayList<org.mujoco.xml.body.GeomType.Builder<?>>());
-						}
-						geomToCSGMap.get(affineNameMapGet).add(geom);
-						parts.add( transformed);
-						setCSGMeshToGeom(geomname, geom);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}else {
-					//println "ERROR! "+name+" for part "+part.getName()+" produced no matching affine"
-				}
-			}
-		}
-		return linkBody;
 	}
+	if(name ==null)
+		throw new RuntimeException("Link name missing from the map!");
+
+	TransformNR location;
+	if(index==0) {
+		location=l.getRobotToFiducialTransform().copy();
+	}else {
+		location=new TransformNR(l.getDhLink(index-1).DhStep(0));
+	}
+	TransformNR local = new TransformNR(l.getDhLink(index).DhStep(0));
+	Transform step = TransformFactory.nrToCSG(local);
+	double x = location.getX()/1000.0;
+
+	double y = location.getY()/1000.0;
+
+	double z = location.getZ()/1000.0;
+	String quat =location.getRotation().getRotationMatrix2QuaturnionW()+" "+
+			location.getRotation().getRotationMatrix2QuaturnionX()+" "+
+			location.getRotation().getRotationMatrix2QuaturnionY()+" "+
+			location.getRotation().getRotationMatrix2QuaturnionZ();
+	TransformNR center = conf.getCenterOfMassFromCentroid();
+	String centerString = center.getX()/1000.0+" "+
+			center.getY()/1000.0+" "+
+			center.getZ()/1000.0+" ";
+	org.mujoco.xml.BodyarchType.Builder<?> linkBody = addBody.addBody()
+			.withName(name)
+			.withPos(x+" "+y+" "+z)
+			.withQuat(quat);
+	//		linkBody.addInertial()
+	//				.withMass(conf.getMassKg())
+	//				.withPos(centerString)
+	//				.withDiaginertia("1 1 1" )
+	linkToBulderMap.put(link, linkBody);
+
+	double gear =460;
+	gearRatios.put(link,1.0d*gear);
+	double upper = Math.toRadians(link.getMaxEngineeringUnits());
+	double lower = Math.toRadians(link.getMinEngineeringUnits());
+	String range=lower+" "+upper;
+	String ctrlRange=(lower*gear)+" "+(upper*gear);
+	double rangeVal=upper-lower;
+	JointType.Builder<?> jointBuilder = linkBody.addJoint();
+	String axisJoint ="0 0 1";
+	///position=0;
+	jointBuilder
+			.withPos("0 0 0")// the kinematic center
+			.withAxis(axisJoint) // rotate about the z axis per dh convention
+			.withRange(ctrlRange) // engineering units range
+			.withRef(BigDecimal.valueOf(0)) // set the reference position on loading as the links 0 degrees value
+			.withType(JointtypeType.HINGE) // hinge type
+			.withFrictionloss(BigDecimal.valueOf(0.01))// experementally determined
+			//.withLimited(true)
+			//.withDamping(BigDecimal.valueOf(0.00001))
+			//.withStiffness(BigDecimal.valueOf(1))
+			.withName(name)
+	;
+	double forceKgCm=3.5;//mg92b default
+	Map<String, Object> configELectroMech =Vitamins.getConfiguration(conf.getElectroMechanicalType(), conf.getElectroMechanicalSize());
+	Object torque =configELectroMech.get("MaxTorqueNewtonmeters");
+	double newtonMeterLimit = 0.0980665*forceKgCm/gear;
+	if(torque!=null) {
+		newtonMeterLimit=Double.parseDouble(torque.toString())/gear;
+	}
+	if (!conf.isPassive()) {
+		
+		actuators.addPosition()// A position controller to model a servo
+				.withKp(BigDecimal.valueOf(0.000006)) // experementally determined value
+				//.withForcelimited(true)
+				//.withForcerange(range)
+				.withCtrlrange(ctrlRange)
+				.withForcerange((-newtonMeterLimit)+" "+newtonMeterLimit)
+				.withName(name)// name the motor
+				.withJoint(name)// which joint this motor powers
+				.withGear(""+gear) // gear ratio between virtual motor and output
+				.withKv(BigDecimal.valueOf(0.00000001)); // damping term experementally determenied
+				//.withInheritrange(BigDecimal.valueOf(rangeVal));// sets the range of the control signal to match the limits
+	}
+	for (int i = 0; i < cad.size(); i++) {
+		CSG part = cad.get(i);
+		if(!checkForPhysics(part))
+			continue;
+		Affine cGetManipulator = part.getManipulator();
+		if(cGetManipulator!=null) {
+			String affineNameMapGet = affineNameMap.get(cGetManipulator);
+			if(affineNameMapGet!=null) {
+				DHParameterKinematics k = l;
+				
+				AbstractLink myLink = mapNameToLink.get(affineNameMapGet);
+
+				double myposition = link.getCurrentEngineeringUnits();
+				TransformNR myStep = new TransformNR(
+						k.getDhLink(myLink).DhStep(0)
+						);
+				CSG transformed = part
+						.transformed(
+						TransformFactory
+						.nrToCSG(
+						myStep
+						))
+						;
+				CSG hull;
+				if(cat.isWheel(myLink)) {
+					double height = part.getTotalZ();
+					double radius =( part.getTotalY()+part.getTotalX())/4.0;
+					hull = new RoundedCylinder(radius, radius, height, 40)
+									.cornerRadius(radius/4)
+									.toCSG()
+									.toZMin()
+									.movez(transformed.getMinZ())
+									;
+				}else
+					hull = transformed.hull();
+				//					if(myLink!=link)
+				//						hull = part.hull();
+				transformed.setManipulator(new Affine());
+				String geomname=name+" "+i;
+
+				try {
+					putCSGInAssets(geomname, hull,true);
+					org.mujoco.xml.body.GeomType.Builder<?> geom = linkToBulderMap.get(myLink).addGeom();
+
+					ArrayList<CSG> parts = getMapNameToCSGParts(affineNameMapGet);
+					if(geomToCSGMap.get(affineNameMapGet)==null) {
+						geomToCSGMap.put(affineNameMapGet, new ArrayList<org.mujoco.xml.body.GeomType.Builder<?>>());
+					}
+					geomToCSGMap.get(affineNameMapGet).add(geom);
+					parts.add( transformed);
+					setCSGMeshToGeom(geomname, geom);
+					if(cat.isWheel(myLink)) {
+						// default is 1 0.005 0.0001
+						//println "Setting Wheel Friction for "+part.getName()
+						geom.withFriction("1.7 0.005 0.0001");
+					}
+					if(cat.isFoot(myLink)) {
+						// default is 1 0.005 0.0001
+						//println "Setting Foot Friction for "+part.getName()
+						geom.withFriction("1.2 0.001 0.00005");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else {
+				//println "ERROR! "+name+" for part "+part.getName()+" produced no matching affine"
+			}
+		}
+	}
+	return linkBody;
+}
 		
 	public  double sig(double x) {
 		if(x>0.999)
