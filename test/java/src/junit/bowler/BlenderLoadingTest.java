9904ff876a4f56a7fef4283cac0fad4e126d7b84
Kevin Harrington <kharrington@bancroftschool.org>
2024-07-05 11:33:36 -0400

    adding the blender loading unit test

---
+++ test/java/src/junit/bowler/BlenderLoadingTest.java
@@ -0,0 +1,23 @@
+package junit.bowler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
+
+import eu.mihosoft.vrl.v3d.CSG;
+
+public class BlenderLoadingTest {
+
+	@Test
+	public void test() throws Exception {
+		CSG loaded =(CSG)ScriptingEngine.gitScriptRun(
+				"https://github.com/madhephaestus/TestRepo.git",
+				"TestRepo4.blend");
+		if(loaded.getPolygons().size()==0)
+			fail("Failed to load polygon!");
+		System.out.println("Blender file loaded num polys: "+loaded.getPolygons().size());
+	}
+
+}
