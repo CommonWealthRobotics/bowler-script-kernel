bd4f81660209c197d06569ae7241d395632d583c
Kevin Harrington <kharrington@bancroftschool.org>
2025-01-07 13:49:07 -0500

    DO not copy the objects

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/CaDoodleFile.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/CaDoodleFile.java
@@ -403,8 +403,9 @@ public class CaDoodleFile {
 				throw new RuntimeException(
 						"There can not be 2 objects with the same name after an opperation! " + c.getName());
 			names.add(c.getName());
-			cachedCopy.add(c.clone().setStorage(new PropertyStorage()).syncProperties(c).setName(c.getName())
-					.setRegenerate(c.getRegenerate()));
+//			cachedCopy.add(c.clone().setStorage(new PropertyStorage()).syncProperties(c).setName(c.getName())
+//					.setRegenerate(c.getRegenerate()));
+			cachedCopy.add(c);
 		}
 		cache.put(op, cachedCopy);
 	}
