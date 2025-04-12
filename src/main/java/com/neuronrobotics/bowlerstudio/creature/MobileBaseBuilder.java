81ec68de96050aa55121421af0330072363d9f7e
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-12 19:43:02 -0400

    initialize the base builder with the giturl

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/MobileBaseBuilder.java
@@ -0,0 +1,13 @@
+package com.neuronrobotics.bowlerstudio.creature;
+
+public class MobileBaseBuilder {
+
+	private String gitURL;
+	private String name;
+
+	public MobileBaseBuilder(String gitURL, String name) {
+		this.gitURL = gitURL;
+		this.name = name;
+		
+	}
+}
