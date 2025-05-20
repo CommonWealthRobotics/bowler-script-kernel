52e2faa9af033c3fe54d54f5b625c25f81929356
Kevin Harrington <mad.hephaestus@gmail.com>
2025-05-20 10:55:20 -0400

    Adding string getters annd setters for the op result for storage in the database as well as adding the ASK option

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/OperationResult.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/OperationResult.java
@@ -1,5 +1,19 @@
 package com.neuronrobotics.bowlerstudio.scripting.cadoodle;
 
 public enum OperationResult {
-	PRUNE,INSERT,ABORT,APPEND;
+	PRUNE, INSERT, ABORT, APPEND, ASK;
+
+	// Static factory method (case-insensitive)
+	public static OperationResult fromString(String name) {
+		for (OperationResult value : OperationResult.values()) {
+			if (value.name().equalsIgnoreCase(name)) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant OperationResult with name: " + name);
+	}
+	@Override
+	public String toString() {
+		return name(); // Or name().toLowerCase(), etc.
+	}
 }
