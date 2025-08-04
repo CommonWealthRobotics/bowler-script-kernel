c2b60119583460fcbe951ba27669f5c05ed39048
Kevin Harrington <mad.hephaestus@gmail.com>
2025-08-04 11:38:55 -0400

    adding basic operataion of make robot

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/robot/MakeRobot.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/robot/MakeRobot.java
@@ -5,10 +5,17 @@ import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.annotations.Expose;
+import com.neuronrobotics.bowlerstudio.creature.MobileBaseBuilder;
 import com.neuronrobotics.bowlerstudio.scripting.cadoodle.AbstractAddFrom;
 import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.parametrics.StringParameter;
 
-public class MakeRobot extends AbstractAddFrom{
+public class MakeRobot extends AbstractAddFrom {
+	@Expose(serialize = true, deserialize = true)
+	private List<String> assignedAsBase = new ArrayList<String>();
+	//@Expose(serialize = true, deserialize = true)
+	MobileBaseBuilder builder;
 
 	@Override
 	public String getType() {
@@ -17,6 +24,19 @@ public class MakeRobot extends AbstractAddFrom{
 
 	@Override
 	public List<CSG> process(List<CSG> incoming) {
+		try {
+			getBuilder().build();
+			for(CSG c:incoming) {
+				for(String s:assignedAsBase) {
+					if(c.getName().contentEquals(s)) {
+						c.setMobileBaseName(getName());
+					}
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return incoming;
 	}
 
@@ -27,8 +47,33 @@ public class MakeRobot extends AbstractAddFrom{
 
 	@Override
 	public File getFile() throws NoSuchFileException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return getBuilder().getFile();
+		} catch (Exception e) {
+			throw new NoSuchFileException(getName());
+		}
+	}
+
+	public List<String> getAssignedAsBase() {
+		return assignedAsBase;
+	}
+
+	public void setNames(List<String> assignedAsBase) {
+		this.assignedAsBase = assignedAsBase;
+	}
+
+	/**
+	 * @return the builder
+	 */
+	public MobileBaseBuilder getBuilder() {
+		if (builder == null) {
+			StringParameter loc = new StringParameter("CaDoodle_File_Location", "NotSet", new ArrayList<String>());
+			String strValue = loc.getStrValue();
+			File parentFile = new File(strValue).getParentFile();
+			String source = parentFile.getAbsolutePath();
+			builder = new MobileBaseBuilder(source, getName() + "-mobilbase.xml");
+		}
+		return builder;
 	}
 
 }
