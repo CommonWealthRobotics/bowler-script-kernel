57805c0dbc7ffc22ab0e721a9d6852418abdd825
Kevin Harrington <mad.hephaestus@gmail.com>
2025-08-06 13:54:24 -0400

    adding more to the options

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/ControllerOption.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/ControllerOption.java
@@ -1,11 +1,14 @@
 package com.neuronrobotics.bowlerstudio.creature;
 
+import static com.neuronrobotics.bowlerstudio.scripting.DownloadManager.delim;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -18,6 +21,15 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.reflect.TypeToken;
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
+import com.neuronrobotics.bowlerstudio.scripting.cadoodle.CaDoodleFile;
+import com.neuronrobotics.bowlerstudio.scripting.cadoodle.robot.AddRobotController;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+
+import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.parametrics.CSGDatabase;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 public class ControllerOption {
 	public static final String URL_OF_OPTIONS = "https://github.com/CommonWealthRobotics/BowlerStudioExampleRobots.git";
@@ -31,6 +43,8 @@ public class ControllerOption {
 	List<String> vitaminType;
 	@Expose(serialize = true, deserialize = true)
 	List<String> vitaminSize;
+	@Expose(serialize = true, deserialize = true)
+	List<TransformNR> vitaminPose;
 	@Expose(serialize = true, deserialize = true)
 	String linkLoaderGit;
 	@Expose(serialize = true, deserialize = true)
@@ -47,8 +61,48 @@ public class ControllerOption {
 	ControllerFeatures provides;
 	@Expose(serialize = true, deserialize = true)
 	ControllerFeatures consumes;
+	
+	// Internal variables
+	private boolean built=false;
+	javafx.scene.image.Image image = null;
+	CSG indicator = null;
+	File stlFile = null;
+	
+	public void build(CaDoodleFile f) {
+		if(built)
+			return;
+		built=true;
+		image =  new Image(getImageFile().toURI().toString());
+		String absolutePath = ScriptingEngine.getWorkspace().getAbsolutePath() + delim() + "uicache";
+		File dir = new File(absolutePath);
+		if (!dir.exists())
+			dir.mkdirs();
+		stlFile = new File(absolutePath + delim() + type + ".stl");
+		if ( stlFile.exists()) {
+			indicator = Vitamins.get(stlFile);
+			getIndicator().setColor(Color.WHITE);
+			return;
+		}else {
+			AddRobotController arc = new AddRobotController().setController(this);
+			arc.setCaDoodleFile(f);
+			List<CSG> so = arc.process(new ArrayList<>());
+			for (CSG c : so) {
+				for (String s : c.getParameters()) {
+					CSGDatabase.delete(s);
+				}
+			}
+			indicator = so.get(0);
+			if (so.size() > 1) {
+				for(int i=1;i<so.size();i++) {
+					indicator=getIndicator().dumbUnion(so.get(i));
+				}
+			}
+			getIndicator().setColor(Color.WHITE);
+		}
 
-	public File getImage() throws FileNotFoundException {
+	}
+
+	public File getImageFile() {
 		try {
 			return ScriptingEngine.fileFromGit(imageGit, imageFile);
 		} catch (InvalidRemoteException e) {
@@ -64,7 +118,7 @@ public class ControllerOption {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		throw new FileNotFoundException();
+		throw new RuntimeException(imageGit+"/"+ imageFile);
 	}
 
 	public void runLinkLoader() throws FileNotFoundException {
@@ -124,16 +178,40 @@ public class ControllerOption {
 	public String getLinkDeviceName() {
 		return linkDeviceName;
 	}
-
+	public CSG getVitaminCSG(int index) {
+		try {
+			return Vitamins.get(vitaminType.get(index), vitaminSize.get(index));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	public List<String> getVitaminType() {
 		return vitaminType;
 	}
-
+	public int getVitaminNumber() {
+		return vitaminSize.size();
+	}
 	public List<String> getVitaminSize() {
 		return vitaminSize;
 	}
 
 	public String getLinkDeviceType() {
 		return linkDeviceType;
+	}
+
+	public javafx.scene.image.Image getImage() {
+		return image;
+	}
+	public TransformNR getVitaminPose(int index) {
+		return getVitaminPose().get(index);
+	}
+	public List<TransformNR> getVitaminPose() {
+		if(vitaminPose==null)
+			return new ArrayList<>(Arrays.asList(new TransformNR()));
+		return vitaminPose;
+	}
+
+	public CSG getIndicator() {
+		return indicator;
 	}
 }
