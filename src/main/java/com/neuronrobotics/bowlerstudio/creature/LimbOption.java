e10196b08594ca2b5cc6e44b04609bfaa7ff5c61
Kevin Harrington <mad.hephaestus@gmail.com>
2025-08-07 10:39:57 -0400

    adding the builder factory class

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/LimbOption.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/LimbOption.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
@@ -16,13 +17,28 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.reflect.TypeToken;
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
 
 public class LimbOption {
 	@Expose(serialize = true, deserialize = true)
 	LimbType type;
 	@Expose(serialize = true, deserialize = true)
 	String name;
+	@Expose(serialize = true, deserialize = true)
+	String url;
+	@Expose(serialize = true, deserialize = true)
+	String file;
+	@Expose(serialize = true, deserialize = true)
+	ControllerFeatures consumes;
+	@Expose(serialize = true, deserialize = true)
+	ControllerFeatures provides;
 	
+	public DHParameterKinematics getLimb() throws Exception {
+		String xmlContent = ScriptingEngine.codeFromGit(url, file)[0];
+		DHParameterKinematics newLimb = new DHParameterKinematics(null, IOUtils.toInputStream(xmlContent, "UTF-8"));
+		newLimb.setScriptingName(name);
+		return newLimb;
+	}
 	public static ArrayList<LimbOption> getOptions()
 			throws InvalidRemoteException, TransportException, GitAPIException, IOException {
 		try {
@@ -37,5 +53,9 @@ public class LimbOption {
 			ex.printStackTrace();
 			return new ArrayList<LimbOption>();
 		}
+	}
+	@Override
+	public String toString() {
+		return type+" "+name+" "+url+"/"+file+"\n\tConsumes:"+consumes+"\n\tProvides:"+provides;
 	}
 }
