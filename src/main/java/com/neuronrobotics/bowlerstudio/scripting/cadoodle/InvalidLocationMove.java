d5f7b9564b8ff4d9fc815780d6423df46715cdc9
Kevin Harrington <mad.hephaestus@gmail.com>
2026-02-03 14:44:00 -0500

    Add a filter on the move operation that rejects origin transforms

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/InvalidLocationMove.java
@@ -0,0 +1,5 @@
+package com.neuronrobotics.bowlerstudio.scripting.cadoodle;
+
+public class InvalidLocationMove extends Exception {
+
+}
--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/MoveCenter.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/MoveCenter.java
@@ -10,13 +10,14 @@ import com.google.gson.annotations.Expose;
 import com.neuronrobotics.bowlerstudio.physics.TransformFactory;
 import com.neuronrobotics.bowlerstudio.vitamins.VitaminBomManager;
 import com.neuronrobotics.sdk.addons.kinematics.VitaminLocation;
+import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR;
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.PropertyStorage;
 import eu.mihosoft.vrl.v3d.Transform;
 
-public class MoveCenter extends CaDoodleOperation{
+public class MoveCenter extends CaDoodleOperation {
 	@Expose(serialize = true, deserialize = true)
 	private TransformNR location = new TransformNR();
 	@Expose(serialize = true, deserialize = true)
@@ -26,7 +27,7 @@ public class MoveCenter extends CaDoodleOperation{
 
 	public String getName() {
 		if (name == null) {
-			name=(RandomStringFactory.generateRandomString());
+			name = (RandomStringFactory.generateRandomString());
 		}
 		return name;
 	}
@@ -35,9 +36,11 @@ public class MoveCenter extends CaDoodleOperation{
 	public String getType() {
 		return "Move Center";
 	}
+
 	public static void set(String name, CSG c, TransformNR tf) {
-		set(name,c,TransformFactory.nrToCSG(tf));
+		set(name, c, TransformFactory.nrToCSG(tf));
 	}
+
 	public static void set(String name, CSG c, Transform tf) {
 		if (tf == null)
 			throw new NullPointerException();
@@ -78,7 +81,8 @@ public class MoveCenter extends CaDoodleOperation{
 					Transform transTmp = new Transform().apply((Transform) storage.getValue(s).get());
 					start = transTmp.apply(start);
 				} catch (Exception ex) {
-					com.neuronrobotics.sdk.common.Log.error(ex);;
+					com.neuronrobotics.sdk.common.Log.error(ex);
+					;
 				}
 			}
 			nrToCSG = start;
@@ -94,31 +98,50 @@ public class MoveCenter extends CaDoodleOperation{
 		CaDoodleFile.applyToAllConstituantElements(false, names, back, new ICadoodleRecursiveEvent() {
 			@Override
 			public ArrayList<CSG> process(CSG incoming, int depth) {
-				
+
 				Transform nrToCSG2 = TransformFactory.nrToCSG(location);
-				if(incoming.isMotionLock()) {
-					nrToCSG2=new Transform();
+				if (incoming.isMotionLock()) {
+					nrToCSG2 = new Transform();
 				}
 				CSG tmpToAdd = incoming.transformed(nrToCSG2)
-						.syncProperties(getCaDoodleFile().getCsgDBinstance(),incoming)
-						.setName(incoming.getName())
+						.syncProperties(getCaDoodleFile().getCsgDBinstance(), incoming).setName(incoming.getName())
 						.setID(incoming);
 				ArrayList<CSG> b = new ArrayList<>();
 				b.add(tmpToAdd);
 				set(getName(), tmpToAdd, location);
 				return b;
 			}
-		},1);
+		}, 1);
 
 		return back;
+	}
+
+	public boolean isWorkplaneNotOrigin(TransformNR w) {
+		double epsilon = 0.00001;
+		RotationNR r = w.getRotation();
+		double abst = Math.abs(w.getX());
+		double abs2t = Math.abs(w.getY());
+		double abs3t = Math.abs(w.getZ());
+
+		if ((abst > epsilon) || (abs2t > epsilon) || (abs3t > epsilon))
+			return true;
+
+		double abs = Math.abs(r.getRotationAzimuthDegrees());
+		double abs2 = Math.abs(r.getRotationElevationDegrees());
+		double abs3 = Math.abs(r.getRotationTiltDegrees());
+
+		return ((abs > epsilon) || (abs2 > epsilon) || (abs3 > epsilon));
 	}
 
 	public TransformNR getLocation() {
 		return location;
 	}
 
-	public MoveCenter setLocation(TransformNR location) {
-		this.location = location;
+	public MoveCenter setLocation(TransformNR location) throws InvalidLocationMove {
+		if (isWorkplaneNotOrigin(location))
+			this.location = location;
+		else
+			throw new InvalidLocationMove();
 		return this;
 	}
 
@@ -126,15 +149,15 @@ public class MoveCenter extends CaDoodleOperation{
 		return names;
 	}
 
-	public MoveCenter setNames(List<String> names,CaDoodleFile f) {
-		
-		for(String s:names) {
-			boolean found=false;
-			for(CSG c:f.getCurrentState()) {
-				if(c.getName().contentEquals(s))
-					found=true;
+	public MoveCenter setNames(List<String> names, CaDoodleFile f) {
+
+		for (String s : names) {
+			boolean found = false;
+			for (CSG c : f.getCurrentState()) {
+				if (c.getName().contentEquals(s))
+					found = true;
 			}
-			if(!found)
+			if (!found)
 				throw new RuntimeException("Set a name that does not exist!!");
 		}
 		this.names = names;
