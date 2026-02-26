dc19d891e0f88891addd80c36ae76b916c333b08
Kevin Harrington <mad.hephaestus@gmail.com>
2026-02-25 20:57:01 -0500

    be more explicate about exceptions

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/SaveOverwriteException.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/SaveOverwriteException.java
@@ -2,4 +2,8 @@ package com.neuronrobotics.bowlerstudio.scripting.cadoodle;
 
 public class SaveOverwriteException extends Exception {
 
+	public SaveOverwriteException(String string) {
+		super(string);
+	}
+
 }
