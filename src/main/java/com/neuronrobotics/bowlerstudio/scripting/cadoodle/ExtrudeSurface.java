5caecb3ce5a2c3304de1c79bc52a8122e9945db4
Kevin Harrington <mad.hephaestus@gmail.com>
2026-05-19 12:40:35 -0400

    formatting

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/ExtrudeSurface.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/ExtrudeSurface.java
@@ -2,12 +2,10 @@ package com.neuronrobotics.bowlerstudio.scripting.cadoodle;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 import com.google.gson.annotations.Expose;
-import com.neuronrobotics.bowlerstudio.assets.ConfigurationDatabase;
 import com.neuronrobotics.bowlerstudio.physics.TransformFactory;
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 import com.neuronrobotics.sdk.common.Log;
@@ -19,8 +17,6 @@ import eu.mihosoft.vrl.v3d.Polygon;
 import eu.mihosoft.vrl.v3d.Slice;
 import eu.mihosoft.vrl.v3d.Transform;
 import eu.mihosoft.vrl.v3d.Vector3d;
-import eu.mihosoft.vrl.v3d.parametrics.LengthParameter;
-import eu.mihosoft.vrl.v3d.parametrics.StringParameter;
 
 public class ExtrudeSurface extends AbstractAddFrom {
 
@@ -60,15 +56,15 @@ public class ExtrudeSurface extends AbstractAddFrom {
 
 	public ArrayList<CSG> makeExtrusion(CSG csgin, String on) {
 		Transform nrToCSG = TransformFactory.nrToCSG(getWorkplane());
-		ArrayList<CSG> fillet=new ArrayList<CSG>();
+		ArrayList<CSG> fillet = new ArrayList<CSG>();
 		try {
 
 			CSG base = csgin.transformed(nrToCSG.inverse()).movez(0.001);
 			List<Polygon> polys = Slice.slice(base);
-			for(Polygon p:polys) {
+			for (Polygon p : polys) {
 				boolean hole = p.isHole();
-				if(hole)
-					p=new Polygon(p.getVertices().reversed());
+				if (hole)
+					p = new Polygon(p.getVertices().reversed());
 				CSG extrude = Extrude.extrude(new Vector3d(0, 0, 20), p);
 				extrude.setIsHole(hole);
 				fillet.add(extrude);
