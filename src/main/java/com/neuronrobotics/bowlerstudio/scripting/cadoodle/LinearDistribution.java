1c5777814e144e814d84bf20ee1068884f9a3678
Kevin Harrington <mad.hephaestus@gmail.com>
2026-05-23 09:27:52 -0400

    set default spacing

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/LinearDistribution.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/LinearDistribution.java
@@ -46,7 +46,7 @@ public class LinearDistribution extends AbstractAddFrom {
 		LengthParameter objectY = new LengthParameter(getDb(), getName() + "_CaDoodle_Y", (double) 3,
 				new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0,
 						15.0, 16.0, 17.0, 18.0, 19.0, 20.0)));
-		LengthParameter rad = new LengthParameter(getDb(), getName() + "_CaDoodle_Spacing", 18.0, new ArrayList<>(
+		LengthParameter rad = new LengthParameter(getDb(), getName() + "_CaDoodle_Spacing", 21.0, new ArrayList<>(
 				Arrays.asList(0.001, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 30.0, 40.0)));
 		StringParameter hexLin = new StringParameter(getDb(), getName() + "_CaDoodle_Hex/Lin", "Honeycomb",
 				new ArrayList<>(Arrays.asList("Honeycomb", "Linear")));
