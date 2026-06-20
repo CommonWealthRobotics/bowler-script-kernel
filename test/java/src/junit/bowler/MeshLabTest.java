e84a264283894a7ea4b87fe6994389a535702f60
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-19 21:22:24 -0400

    formatting

--- test/java/src/junit/bowler/MeshLabTest.java
+++ test/java/src/junit/bowler/MeshLabTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.util.Optional;
 
 import org.junit.Test;
 
@@ -20,47 +18,47 @@ public class MeshLabTest {
 
 	@Test
 	public void test() {
-		 File exe = DownloadManager.getRunExecutable("meshlab", new IExternalEditor() {
-			
+		File exe = DownloadManager.getRunExecutable("meshlab", new IExternalEditor() {
+
 			@Override
 			public void onProcessExit(int ev) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public String nameOfEditor() {
 				// TODO Auto-generated method stub
 				return null;
 			}
-			
+
 			@Override
 			public void launch(File file, Button advanced, Runnable onExit) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public Class getSupportedLangauge() {
 				// TODO Auto-generated method stub
 				return null;
 			}
-			
+
 			@Override
 			public URL getInstallURL() throws MalformedURLException {
 				// TODO Auto-generated method stub
 				return null;
 			}
-			
+
 			@Override
 			public Image getImage() {
 				// TODO Auto-generated method stub
 				return null;
 			}
 		});
-		if(!exe.exists())
+		if (!exe.exists())
 			fail("Failed to fine MeshLab Executable");
-		System.out.println("Mesh lab found: "+exe.getAbsolutePath());
+		System.out.println("Mesh lab found: " + exe.getAbsolutePath());
 	}
 
 }
