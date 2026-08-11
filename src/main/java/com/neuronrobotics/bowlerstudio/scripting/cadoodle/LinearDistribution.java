2ade9e6b6885b75b70865db99fccc8152bcee2dc
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-11 10:31:10 -0400

    increase linear distribuion cap

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/LinearDistribution.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/LinearDistribution.java
@@ -42,13 +42,13 @@ public class LinearDistribution extends AbstractAddFrom {
 		index = 1;
 		LengthParameter objectX = new LengthParameter(getDb(), getName() + "_CaDoodle_X", (double) 3,
 				new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0,
-						15.0, 16.0, 17.0, 18.0, 19.0, 20.0)));
+						15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 500.0)));
 		LengthParameter objectY = new LengthParameter(getDb(), getName() + "_CaDoodle_Y", (double) 3,
 				new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0,
-						15.0, 16.0, 17.0, 18.0, 19.0, 20.0)));
+						15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 500.0)));
 		LengthParameter objectZ = new LengthParameter(getDb(), getName() + "_CaDoodle_Z", (double) 1,
 				new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0,
-						15.0, 16.0, 17.0, 18.0, 19.0, 20.0)));
+						15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 500.0)));
 		LengthParameter rad = new LengthParameter(getDb(), getName() + "_CaDoodle_Spacing", 21.0,
 				new ArrayList<>(Arrays.asList(0.001, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 30.0,
 						40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 1000.0)));
