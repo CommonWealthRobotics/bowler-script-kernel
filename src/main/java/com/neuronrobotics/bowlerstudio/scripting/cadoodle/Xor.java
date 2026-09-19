b282bab7ff9b893528e3284e447f2763ad0906ec
Kevin Harrington <mad.hephaestus@gmail.com>
2026-09-19 12:33:25 -0400

    Add the internal Xor operation as a non-group because there are more than one results breaking the grouping assumptions.

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/Xor.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/cadoodle/Xor.java
@@ -3,11 +3,18 @@ package com.neuronrobotics.bowlerstudio.scripting.cadoodle;
 import java.io.File;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import com.google.gson.annotations.Expose;
+import com.neuronrobotics.bowlerstudio.physics.TransformFactory;
 
 import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.MissingManipulatorException;
+import eu.mihosoft.vrl.v3d.PrepForManufacturing;
+import eu.mihosoft.vrl.v3d.parametrics.IParametric;
+import javafx.scene.transform.Affine;
 
 public class Xor extends AbstractAddFrom {
 	@Expose(serialize = true, deserialize = true)
@@ -35,42 +42,74 @@ public class Xor extends AbstractAddFrom {
 	@Override
 	public List<CSG> process(List<CSG> incoming) {
 		ArrayList<CSG> back = new ArrayList<>();
+		ArrayList<CSG> toXor = new ArrayList<>();
+		boolean noscale = false;
+		Affine manip = null;
+		boolean nomove = false;
+		PrepForManufacturing mfg = null;
+		String mobileBase = null;
 
-		//		Paste copy = new Paste().setNames(names);
-		//		copy.process(incoming);
-		//		ArrayList<String> n = new ArrayList<>(copy.getNamesAddedInThisOperation());
-		//		Group groups = new Group().setNames(n);
-		//		groups.setHull(false);
-		//		groups.setIntersect(true);
-		//		groups.process(incoming);
-		//		List<CSG> results = ap.get().getCurrentState();
-		//		String intersectName = groups.getGroupID();
-		//		ArrayList<String> names = new ArrayList<>();
-		//		names.add(intersectName);
-		//		ToHole th = new ToHole().setNames(names);
-		//		ap.addOp(th).join();
-		//
-		//		for (int i = 0; i < n.size(); i++) {
-		//			String e = n.get(i);
-		//			ap.get();
-		//			CSG g = null;
-		//
-		//			try {
-		//				g = CaDoodleFile.getByName(ap.get().getCurrentState(), e);
-		//			} catch (NameMissingException e1) {
-		//				continue;
-		//			}
-		//
-		//			if ((g == null) || g.isInGroup())
-		//				continue;
-		//
-		//			ArrayList<String> namesToDiff = new ArrayList<String>();
-		//			namesToDiff.add(intersectName);
-		//			namesToDiff.add(e);
-		//			Group cutIntersect = new Group().setNames(namesToDiff);
-		//			ap.addOp(cutIntersect).join();
-		//
-		//		}
+		for (CSG c : incoming) {
+
+			for (String s : names) {
+				if (c.getName().contentEquals(s)) {
+					if (c.isNoScale())
+						noscale = true;
+					if (c.isMotionLock())
+						nomove = true;
+					Optional<String> mobileBaseName = c.getMobileBaseName();
+					if (mobileBaseName.isPresent()) {
+						if (mobileBase == null)
+							mobileBase = mobileBaseName.get();
+						if (!mobileBase.contentEquals(mobileBaseName.get())) {
+							continue;// skip grouping any item that is of a different mobile base;
+						}
+					}
+					if (c.hasManipulator()) {
+						try {
+							manip = c.getManipulator();
+							c = c.transformed(TransformFactory.nrToCSG(TransformFactory.affineToNr(manip)))
+									.syncProperties(getCaDoodleFile().getCsgDBinstance(), c)
+									.setRegenerate(c.getRegenerate()).setName(name);
+
+						} catch (MissingManipulatorException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					if (c.hasManufacturing()) {
+						mfg = c.getManufacturing();
+					}
+					toXor.add(c);
+				} else
+					back.add(c);
+			}
+		}
+		CSG intersection = Group.intersect(toXor);
+		for (int i = 0; i < toXor.size(); i++) {
+			CSG f = toXor.get(i);
+			CSG result = f.difference(intersection);
+			if (manip != null) {
+				result = result.transformed(TransformFactory.nrToCSG(TransformFactory.affineToNr(manip).inverse()));
+				result.setManipulator(manip);
+			}
+			if (mfg != null)
+				result.setManufacturing(mfg);
+			if (mobileBase != null)
+				result.setMobileBaseName(mobileBase);
+			HashMap<String, IParametric> mapOfparametrics = result
+					.getMapOfparametrics(getCaDoodleFile().getCsgDBinstance());
+			if (mapOfparametrics != null)
+				mapOfparametrics.clear();
+			result.setName(getOperationID() + "_" + i);
+			result.setUserDefinedName("Xor " + i);
+			result.setNoScale(noscale);
+			result.setIsMotionLock(nomove);
+			result.setIsAlwaysShow(false);
+			addNameInThisOperation(result.getName());
+			back.add(result);
+		}
+
 		return back;
 	}
 
